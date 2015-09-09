e84447b73ac9d50658acb77772d33fe8da3413ca
Miguel Mendoza <migue_060195@ciencias.unam.mx>
2015-09-09 14:43:24 -0500

    Fixin merge

--- Practicas/practica2/practica2.rkt
+++ Practicas/practica2/practica2.rkt
@@ -19,4 +19,4 @@
   (NodeN (node number?) (children (listof NTree?))))
 
 (test (TLEmpty) (TLEmpty))
-(test (NodeN 1 (list (TLEmpty) (TLEmpty) (TLEmpty)))  (NodeN 1 (list (TLEmpty) (TLEmpty) (TLEmpty))) )
+(test (NodeN 1 (list (TLEmpty) (TLEmpty) (TLEmpty)))  (NodeN 1 (list (TLEmpty) (TLEmpty) (TLEmpty))))
