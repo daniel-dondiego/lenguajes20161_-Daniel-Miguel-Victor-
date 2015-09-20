29c5a4b147aaae81cd1f8ab00e074b3488a72c31
daniel-dondiego <rdondiego@ciencias.unam.mx>
2015-09-20 10:39:12 -0500

    agrega nlBT

--- Practicas/practica3/practica3.rkt
+++ Practicas/practica3/practica3.rkt
@@ -1,3 +1,21 @@
 #lang plai
 
 (require "practica3-base.rkt")
+
+
+;;nlBT 
+;;Dado un árbol de tipo BTree, determinar el número de hojas no vacías.
+(define (nlBT tree)
+  (cond
+    [(EmptyBT? tree) 0] ;; caso base 
+    [(and (EmptyBT? (BNode-l tree)) (EmptyBT? (BNode-r tree))) 1] ;; si ambos nodos hijos son vacíos entonces es hoja y es no vacía ya que no cayó en el caso anterior asi que la sumamos 
+    [else (+ (nlBT (BNode-l tree)) (nlBT(BNode-r tree)) )])) ;; recursa sobre cada sub arbol y suma el resultado de estos
+
+;;tests para nlBT 
+(test (nlBT arb1) 1)
+(test (nlBT arb2) 2)
+(test (nlBT arb3) 4)
+(test (nlBT arb4) 8)
+(test (nlBT (bnn arb4 5 arb4)) 16)
+
+
