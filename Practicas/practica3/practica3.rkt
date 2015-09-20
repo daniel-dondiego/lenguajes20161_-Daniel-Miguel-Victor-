b566ab3b9d6b6ae4bb7f0ed5142959677cf20406
Victor Serrato <vicserratos@ciencias.unam.mx>
2015-09-19 21:51:01 -0500

    Se ha agregado la funcion get-zone y sus tests y los test para zone

--- Practicas/practica3/practica3.rkt
+++ Practicas/practica3/practica3.rkt
@@ -1,3 +1,48 @@
 #lang plai
 
 (require "practica3-base.rkt")
+
+; Seccion 1
+
+;Se define para ejemplos
+(define my zones (zones 50 180))
+
+;Dada una zona y una lista regresa el tipo de dato correspondiente
+(define (get-zone symbol lst)
+  (cond
+    [(eq? symbol 'resting) (list-ref lst 0)]
+    [(eq? symbol 'warm-up) (list-ref lst 1)]
+    [(eq? symbol 'fat-burning) (list-ref lst 2)]
+    [(eq? symbol 'aerobic) (list-ref lst 3)]
+    [(eq? symbol 'anaerobic) (list-ref lst 4)]
+    [(eq? symbol 'maximum) (list-ref lst 5)]
+    [else (error "El simbolo no esta en la lista")]))
+
+     
+     
+     
+;Tests
+
+;Zones
+(test (zones 50 180)
+      (list
+       (resting 50 114.0)
+       (warm-up 115.0 127.0)
+       (fat-burning 128.0 140.0)
+       (aerobic 141.0 153.0)
+       (anaerobic 154.0 166.0)
+       (maximum 167.0 180)))
+
+;Tests para get-zone
+(test (get-zone 'resting my-zones) 
+      (resting 50 114.0))
+(test (get-zone 'warm-up my-zones)
+      (warm-up 115.0 127.0))
+(test (get-zone 'fat-burning my-zones)
+      (fat-burning my-zones))
+(test (get-zone 'aerobic my-zones)
+      (aerobic 141.0 153.0))
+(test (get-zone 'anaerobic my-zones)
+      (anaerobic 154.0 166.0))
+(test (get-zone 'maximum my-zones)
+      (maximum 167.0 180))
